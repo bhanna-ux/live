47f453d210bc0a74db390debdb4b8ecc132fb520
bhanna-ux <114748994+bhanna-ux@users.noreply.github.com>
2025-02-09 21:51:17 +0200

    Add files via upload

---
+++ live transcribe.py
@@ -0,0 +1,133 @@
+import streamlit as st
+import subprocess
+import ffmpeg
+import numpy as np
+import soundfile as sf
+import librosa
+import time
+import threading
+import scipy.io.wavfile as wavfile
+import os
+import signal
+from streamlink import Streamlink
+import whisper
+import itertools
+import multiprocessing
+from multiprocessing import Process
+from subprocess import Popen, PIPE
+
+from threading import Event
+import datetime
+
+
+
+
+st.set_page_config(layout="wide")
+model=whisper.load_model('tiny')
+url = st.sidebar.text_input(':blue[Add Live youtube URL here:]')
+def stream_to_url(url, quality='best'):
+    # The "audio_only" quality may be invalid for some streams (check).
+    session = Streamlink()
+    streams = session.streams(url)
+    return streams[quality].to_url()
+
+
+#class Record:
+    #def __init__(self):
+        #self.ffmpeg_process = None
+d=[]
+def oneFunction():
+
+
+    stream_url = stream_to_url(url)
+    fmpeg_process = (ffmpeg
+.input(stream_url)
+.audio
+.output('/kaggle/working/recording.wav')
+.overwrite_output()
+.run_async())
+    st.write(":red[RECORDING Audio ....]",height=30)
+    def Finish():
+
+        fmpeg_process.send_signal(signal.SIGQUIT)
+        st.write(':red[Recording FINISHED ..... ]',height=30)
+        st.audio('/kaggle/working/recording.wav')
+        st.text_area(label=' Transcript : ', value= d ,height=200)
+
+    st.sidebar.button("Finish Recording ",on_click=Finish)
+
+        #play_audio=st.audio("/content/recording.wav")
+        #st.text_area(label=' Transcript : ', value= text ,height=200)
+        #ffmpeg_process.send_signal(signal.CTRL_C_EVENT)
+
+
+
+
+def done():
+
+    lasttimeinterval=0
+
+
+    for i in itertools.count():
+        audio_array, sr = librosa.load("/kaggle/working/recording.wav", sr=16_000)
+        timeinterval=len(audio_array)/sr
+        t=audio_array[int(lasttimeinterval)*sr : int(timeinterval)*sr]
+        sf.write("/kaggle/working/split_interval.wav", t, sr)
+        result=model.transcribe("/kaggle/working/split_interval.wav" )
+        if result['text']:
+
+            #translated = GoogleTranslator(source='auto', target='en').translate(result['text'])
+
+            yield str(datetime.timedelta(seconds=int(lasttimeinterval))) , result['text'] #st.text_area(label=' Transcript : ', value= result['text'] ,height=200)
+            d.append(result['text'])
+        yield "\n"
+        #yield translated
+        #with col2:
+                #yield st.write_stream(translated)
+
+        lasttimeinterval=timeinterval
+
+
+
+
+
+    #st.text_area(label=' Translation : ', value= translation,height=200)
+if url :
+    play_file=st.sidebar.video(url)
+record=st.sidebar.button(":blue[Record Audio and transcribe  ]" ,use_container_width=True)
+transcribe=st.sidebar.button(":blue[ live  Transcribe  ]" ,use_container_width=True)
+#stop=st.sidebar.button(":blue[Stop  ]" ,use_container_width=True)
+if st.sidebar.button("Play Recorded Audio"):
+    st.write("Recorded File  " )
+    video_file = open('/kaggle/working/recording.wav', "rb")
+    video_bytes = video_file.read()
+    st.audio(video_bytes)
+
+
+if st.sidebar.button('show transcription :'):
+    st.text_area(label=' Transcript : ', value= d ,height=200)
+if record :
+
+
+    oneFunction()
+    #done
+    #recorder.start_recording(url)
+
+
+        #oneFunction.stop(fmpeg_process)
+if transcribe:
+  done
+#stop=st.sidebar.button(":blue[Stop  ]" ,use_container_width=True)
+#if stop:
+  #recorder.stop_recording()
+#if stop :
+    #b.stop_recording()
+
+
+
+
+
+    #stop=st.sidebar.button(":blue[Stop  ]" ,use_container_width=True)
+    #if stop :
+        #p1.join(1)
+        #fmpeg_process.send_signal(signal.SIGINT)
